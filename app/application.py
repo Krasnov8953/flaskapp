ec17076b6aa7a6c0f5378abfc458ed5abff200ab
krasnov8953 <v.krasnov@pr-solution.ru>
2018-10-04 22:48:19 +0300

    initialize configuration

--- app/application.py
+++ app/application.py
@@ -1,22 +1,22 @@
+import os
 from flask import Flask
-from flask_dotenv import DotEnv
 from .model import db
+from dotenv import load_dotenv
 
 __all__ = ['create_app']
 
 
-def create_app():
+def create_app(config=None):
+    load_dotenv()   
     app = Flask(__name__)
-    initialize_extensions(app)
+    # initialize_extensions(app)
     initialize_blueprints(app)
+    initialize_configuration(app)
     return app
 
 
-def initialize_extensions(app):
-    env = DotEnv()
-
-    db.init_app(app)
-    env.init_app(app)
+# def initialize_extensions(app):
+#     db.init_app(app)
 
 
 def initialize_blueprints(app):
@@ -25,3 +25,20 @@ def initialize_blueprints(app):
 
     for bp in [main]:
         app.register_blueprint(bp)
+
+def initialize_configuration(app):
+    import json
+
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    env = os.getenv('ENV_TYPE')
+
+    if env == 'dev':
+        app.config = config['DEVELOPMENT']
+    elif env == 'test':
+        app.config = config['TEST']
+    elif env == 'prod':
+        app.config = config['PRODUCTION']
+    else:
+        raise ValueError('Invalid environment name')
